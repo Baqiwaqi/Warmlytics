fb9e94600c637073b60a422cc5e78b33bbfda737
Tim <48436110+Baqiwaqi@users.noreply.github.com>
2023-03-12 17:47:28 +0100

    update styling

---
+++ src/utils/formatters.ts
@@ -0,0 +1,4 @@
+export const numberFormatter2 = new Intl.NumberFormat('nl-NL', {
+   minimumFractionDigits: 2,
+   maximumFractionDigits: 2,
+});
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,30 @@
 const config = {
    content: ["./src/**/*.{js,ts,jsx,tsx}"],
    theme: {
-      extend: {},
+      extend: {
+         fontFamily: {
+            sans: ["Inter", "sans-serif"],
+         },
+      },
    },
    plugins: [require("daisyui")],
    daisyui: {
       themes: [
-         "lemonade",
+         // "lemonade",
          // "forest",
+         {
+            mytheme: {
+               "primary": "#6366f1",
+               "secondary": "#fb7185",
+               "accent": "#37CDBE",
+               "neutral": "#3D4451",
+               "base-100": "#FFFFFF",
+               "info": "#3ABFF8",
+               "success": "#36D399",
+               "warning": "#FBBD23",
+               "error": "#F87272",
+            },
+         },
       ],
    },
 };
